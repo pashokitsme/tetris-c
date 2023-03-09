1e4db9d52fec093b51e6d2180387c2ea44aa62b0
pashokitsme <pashamail79@gmail.com>
2023-03-09 03:02:58 +0300

    Tick delay

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#include "global.h"
-#include "game.h"
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 #include "field.h"
+#include "game.h"
+#include "global.h"
 
 int main() {
   GameState state = init();
@@ -13,16 +19,22 @@ int main() {
 #if DEBUG
     printf("\ninfo > frame #%zu\n\n", state.tick);
 #endif
-    
+
     clear_frame(state.buf);
     tick(&state);
     draw(state.buf);
+
+#if defined(_WIN32) || defined(_WIN64)
+    Sleep(1000);
+#else
+    sleep(1)
+#endif
   }
-  
+
   return 0;
 }
 
-/* 
+/*
   char **buf = alloc_new_frame();
   init_frame(buf);
   while (true) {
